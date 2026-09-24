655a705792eef54fa498f7437669f684cba1b52c
Nwafor Chukwunonso Evaristus <kizitonwafor7@gmail.com>
2026-09-24 11:07:59 +0100

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://3.21.52.54:8080',
+    baseURL:'http://3.21.52.54:8080/movies',
     headers: {
         'Content-Type': 'application/json',
     },
